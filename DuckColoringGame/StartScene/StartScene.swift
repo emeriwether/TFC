9da5a9ef98267a88dcf525d79de34365705e8b9b
Gustavo Figueroa <gcfiguer@uci.edu>
2018-01-26 12:57:17 -0800

    Added the Coloring Game Trainers Back

--- DuckColoringGame/StartScene/StartScene.swift
+++ DuckColoringGame/StartScene/StartScene.swift
@@ -38,7 +38,7 @@ class StartScene: SKScene {
         
         // if the start button is touched, move to duck scene
         if startColor.contains(touch.location(in: self)) {
-            let duckScene = SKScene(fileNamed: "DuckScene")
+            let duckScene = SKScene(fileNamed: "Trainer_Egg")
             duckScene?.scaleMode = SKSceneScaleMode.aspectFill
             self.scene!.view?.presentScene(duckScene!, transition: fade)
         }
