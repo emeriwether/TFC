d0b6dab7a60ae30f9aabe3e38347a9d26c0076da
Gustavo Figueroa <gcfiguer@uci.edu>
2018-01-26 14:15:21 -0800

    Added Egg Scene

---
+++ DuckColoringGame/ColoringGame_Trainer/Trainer_Egg.swift
@@ -0,0 +1,13 @@
+//
+//  Trainer_Egg.swift
+//  DuckColoringGame
+//
+//  Created by Gustavo C Figueroa on 1/26/18.
+//  Copyright © 2018 Eleanor Meriwether. All rights reserved.
+//
+
+import UIKit
+
+class Trainer_Egg: SKScene {
+
+}
