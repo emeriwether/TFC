255b3ca0fc2518c3813aee3b9d4cabb9c9d21500
Eleanor Meriwether <eleanor@delux.com>
2018-02-07 15:30:52 -0800

    work on navigation for game

--- DuckColoringGame/GameScene/GameScene.swift
+++ DuckColoringGame/GameScene/GameScene.swift
@@ -3,51 +3,32 @@ import SpriteKit
 
 class GameScene: SKScene {
 
-    var closeButton:SKNode!
-  
+    var navigation:SKNode!
+    var closeButton: SKSpriteNode!
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+        navigation = childNode(withName: "navigation")
+        closeButton = childNode(withName: "//closeButton") as! SKSpriteNode
+    }
+    
     func goToScene(scene: SKScene) {
         let sceneTransition = SKTransition.fade(with: UIColor.darkGray, duration: 1)
         scene.scaleMode = .aspectFill
         self.view?.presentScene(scene, transition: sceneTransition)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        touchMoved(to: touch.location(in: self))
-    }
-  
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        touchUp(at: touch.location(in: self))
-    }
-  
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        touchUp(at: touch.location(in: self))
-    }
-  
-    // MARK:- Stub methods - override these in sub classes
-    func touchDown(at point: CGPoint) {}
-    func touchMoved(to point: CGPoint) {}
-    func touchUp(at point: CGPoint) {}
-    func getNextScene() -> SKScene? {
-        return nil
-    }
-    
-    func getPreviousScene() -> SKScene? {
-        return nil
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-        
         let touchLocation = touch.location(in: self)
-        let fade = SKTransition.fade(withDuration: 0.5)
         
-        if closeButton.contains(touchLocation) {
-            let startScene = SKScene(fileNamed: "StartScene")
-            startScene?.scaleMode = SKSceneScaleMode.aspectFill
-            self.scene!.view?.presentScene(startScene!, transition: fade)
+        if navigation.contains(touchLocation) {
+            let location = touch.location(in: navigation)
+            if closeButton.contains(location) {
+                let scoreScene = SKScene(fileNamed: "ScoreScene")
+                scoreScene?.scaleMode = SKSceneScaleMode.aspectFill
+                self.scene!.view?.presentScene(scoreScene!)
+            }
         }
     }
 }
