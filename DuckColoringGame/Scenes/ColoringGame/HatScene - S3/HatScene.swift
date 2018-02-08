a8a0599a43816e94f76dddae6faaaeb045d56497
Eleanor Meriwether <eleanor@delux.com>
2018-02-08 15:59:46 -0800

    add hat scene & modify pig in mouse scene to delete whitespace in tail

--- DuckColoringGame/Scenes/ColoringGame/HatScene - S3/HatScene.swift
+++ DuckColoringGame/Scenes/ColoringGame/HatScene - S3/HatScene.swift
@@ -1,5 +1,5 @@
 //
-//  TrainScene.swift
+//  HatScene.swift
 //  TimeForChildrenGame
 //
 //  Created by Eleanor Meriwether on 12/7/17.
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class TrainScene: SKScene {
+class HatScene: SKScene {
     // local variables to keep track of whether instructions are playing
     var instructionsComplete = false
     var reminderComplete = true
@@ -17,8 +17,8 @@ class TrainScene: SKScene {
     var sceneOver = false
     
     // local variables to keep track of touches for this scene
-    var train_incorrectTouches = 0
-    var train_correctTouches = 0
+    var hat_incorrectTouches = 0
+    var hat_correctTouches = 0
     var totalTouches = 0
     
     override func didMove(to view: SKView) {
@@ -39,8 +39,8 @@ class TrainScene: SKScene {
         // set up sequence for if the scene has not been touched for 10 seconds: play the idle reminder
         let reminderIfIdle = SKAction.run {
             self.reminderComplete = false
-            let train_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
-            self.run(train_reminder, completion: { self.reminderComplete = true} )
+            let hat_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
+            self.run(hat_reminder, completion: { self.reminderComplete = true} )
         }
         
         // for every one second, do this action:
@@ -71,57 +71,57 @@ class TrainScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // local variable for train sprite
-        let train = self.childNode(withName: "train_bw")
+        // local variable for hat sprite
+        let hat = self.childNode(withName: "hat_bw")
         
         // if no instructions are playing
         if (instructionsComplete == true) && (reminderComplete == true) && (sceneOver == false){
             let touch = touches.first!
             
-            //If train sprite's alpha mask is touched...
-            if (physicsWorld.body(at: touch.location(in: self)) == train?.physicsBody) && (sceneOver == false) {
+            //If hat sprite's alpha mask is touched...
+            if (physicsWorld.body(at: touch.location(in: self)) == hat?.physicsBody) && (sceneOver == false) {
                 sceneOver = true
-                train_correctTouches += 1
+                hat_correctTouches += 1
                 correctTouches += 1
                 
-                // Change sprite to colored duck
-                let coloredTrain:SKTexture = SKTexture(imageNamed: "trainScene_train_colored")
-                let changeToColored:SKAction = SKAction.animate(with: [coloredTrain], timePerFrame: 0.0001)
-                train!.run(changeToColored)
-            
-                //Variables for Train audio
-                let chooChoo = SKAction.playSoundFileNamed("quack", waitForCompletion: true)
+                // Change sprite to colored hat
+                let coloredHat:SKTexture = SKTexture(imageNamed: "hatScene_hat_colored")
+                let changeToColored:SKAction = SKAction.animate(with: [coloredHat], timePerFrame: 0.0001)
+                hat!.run(changeToColored)
+                
+                //Variables for hat audio
+                let western = SKAction.playSoundFileNamed("quack", waitForCompletion: true)
                 //Variables for move animation
                 let move = SKAction.moveTo(x: 900, duration: 3.0)
-
+                
                 //Run all actions
-                train!.run(chooChoo)
-                train!.run(move)
+                hat!.run(western)
+                hat!.run(move)
                 
                 //Variables to switch screens
                 let fadeOut = SKAction.fadeOut(withDuration:2)
                 let wait2 = SKAction.wait(forDuration: 2)
                 let sequenceFade = SKAction.sequence([wait2, fadeOut])
                 run(sequenceFade) {
-                    let hatScene = SKScene(fileNamed: "HatScene")
-                    hatScene?.scaleMode = SKSceneScaleMode.aspectFill
-                    self.scene!.view?.presentScene(hatScene!)
+                    let scoreScene = SKScene(fileNamed: "ScoreScene")
+                    scoreScene?.scaleMode = SKSceneScaleMode.aspectFill
+                    self.scene!.view?.presentScene(scoreScene!)
                 }
             }
             else {
-                train_incorrectTouches += 1
+                hat_incorrectTouches += 1
                 incorrectTouches += 1
             }
-
+            
             // play reminder instructions if user has touched screen 3 times incorrectly
-            if (train_incorrectTouches % 3 == 0) && train_correctTouches < 1 {
+            if (hat_incorrectTouches % 3 == 0) && hat_correctTouches < 1 {
                 reminderComplete = false
-                let train_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
-                run(train_reminder, completion: { self.reminderComplete = true} )
+                let hat_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
+                run(hat_reminder, completion: { self.reminderComplete = true} )
             }
         }
         // update totalTouches variable for idle reminder
-        totalTouches = train_correctTouches + train_incorrectTouches
+        totalTouches = hat_correctTouches + hat_incorrectTouches
     }
 }
 
