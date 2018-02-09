fe0b8b0495b1910993958bf089a78264b3433237
Eleanor Meriwether <eleanor@delux.com>
2018-02-08 16:55:36 -0800

    update pizza and hamburger scenes to match other coloring scenes & add in second sprite for each of the two scenes

--- DuckColoringGame/Scenes/ColoringGame/HamburgerScene - B2/HamburgerScene.swift
+++ DuckColoringGame/Scenes/ColoringGame/HamburgerScene - B2/HamburgerScene.swift
@@ -1,5 +1,5 @@
 //
-//  PizzaScene.swift
+//  HamburgerScene.swift
 //  TimeForChildrenGame
 //
 //  Created by Eleanor Meriwether on 12/7/17.
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class PizzaScene: SKScene {
+class HamburgerScene: SKScene {
     // local variables to keep track of whether instructions are playing
     var instructionsComplete = false
     var reminderComplete = true
@@ -17,8 +17,8 @@ class PizzaScene: SKScene {
     var sceneOver = false
     
     // local variables to keep track of touches for this scene
-    var pizza_incorrectTouches = 0
-    var pizza_correctTouches = 0
+    var hamburger_incorrectTouches = 0
+    var hamburger_correctTouches = 0
     var totalTouches = 0
     
     override func didMove(to view: SKView) {
@@ -39,8 +39,8 @@ class PizzaScene: SKScene {
         // set up sequence for if the scene has not been touched for 10 seconds: play the idle reminder
         let reminderIfIdle = SKAction.run {
             self.reminderComplete = false
-            let pizza_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
-            self.run(pizza_reminder, completion: { self.reminderComplete = true} )
+            let hamburger_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
+            self.run(hamburger_reminder, completion: { self.reminderComplete = true} )
         }
         
         // for every one second, do this action:
@@ -71,57 +71,57 @@ class PizzaScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // local variable for pizza sprite
-        let pizza = self.childNode(withName: "pizza_bw")
+        // local variable for hamburger sprite
+        let hamburger = self.childNode(withName: "hamburger_bw")
         
         // if no instructions are playing
         if (instructionsComplete == true) && (reminderComplete == true) && (sceneOver == false){
             let touch = touches.first!
             
-            //If pizza sprite's alpha mask is touched...
-            if (physicsWorld.body(at: touch.location(in: self)) == pizza?.physicsBody) && (sceneOver == false) {
+            //If hamburger sprite's alpha mask is touched...
+            if (physicsWorld.body(at: touch.location(in: self)) == hamburger?.physicsBody) && (sceneOver == false) {
                 sceneOver = true
-                pizza_correctTouches += 1
+                hamburger_correctTouches += 1
                 correctTouches += 1
                 
-                // Change sprite to colored pizza
-                let coloredPizza:SKTexture = SKTexture(imageNamed: "pizzaScene_pizza_colored")
-                let changeToColored:SKAction = SKAction.animate(with: [coloredPizza], timePerFrame: 0.0001)
-                pizza!.run(changeToColored)
+                // Change sprite to colored hamburger
+                let coloredhamburger:SKTexture = SKTexture(imageNamed: "hamburgerScene_hamburger_colored")
+                let changeToColored:SKAction = SKAction.animate(with: [coloredhamburger], timePerFrame: 0.0001)
+                hamburger!.run(changeToColored)
                 
-                //Variables for pizza audio
+                //Variables for hamburger audio
                 let western = SKAction.playSoundFileNamed("quack", waitForCompletion: true)
                 //Variables for move animation
                 let move = SKAction.moveTo(x: 900, duration: 3.0)
                 
                 //Run all actions
-                pizza!.run(western)
-                pizza!.run(move)
+                hamburger!.run(western)
+                hamburger!.run(move)
                 
                 //Variables to switch screens
                 let fadeOut = SKAction.fadeOut(withDuration:2)
                 let wait2 = SKAction.wait(forDuration: 2)
                 let sequenceFade = SKAction.sequence([wait2, fadeOut])
                 run(sequenceFade) {
-                    let pizzaScene = SKScene(fileNamed: "PizzaScene")
-                    pizzaScene?.scaleMode = SKSceneScaleMode.aspectFill
-                    self.scene!.view?.presentScene(pizzaScene!)
+                    let scoreScene = SKScene(fileNamed: "ScoreScene")
+                    scoreScene?.scaleMode = SKSceneScaleMode.aspectFill
+                    self.scene!.view?.presentScene(scoreScene!)
                 }
             }
             else {
-                pizza_incorrectTouches += 1
+                hamburger_incorrectTouches += 1
                 incorrectTouches += 1
             }
             
             // play reminder instructions if user has touched screen 3 times incorrectly
-            if (pizza_incorrectTouches % 3 == 0) && pizza_correctTouches < 1 {
+            if (hamburger_incorrectTouches % 3 == 0) && hamburger_correctTouches < 1 {
                 reminderComplete = false
-                let pizza_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
-                run(pizza_reminder, completion: { self.reminderComplete = true} )
+                let hamburger_reminder = SKAction.playSoundFileNamed("reminder_duck", waitForCompletion: true)
+                run(hamburger_reminder, completion: { self.reminderComplete = true} )
             }
         }
         // update totalTouches variable for idle reminder
-        totalTouches = pizza_correctTouches + pizza_incorrectTouches
+        totalTouches = hamburger_correctTouches + hamburger_incorrectTouches
     }
 }
 
