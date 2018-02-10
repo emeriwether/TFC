2750eccd14301e2f1dbe751b030eabafccc675f7
Eleanor Meriwether <eleanor@delux.com>
2018-02-10 14:08:30 -0800

    collect data from text field for validation for password

--- DuckColoringGame/Scenes/EndGameScenes/PasswordScene/PasswordScene.swift
+++ DuckColoringGame/Scenes/EndGameScenes/PasswordScene/PasswordScene.swift
@@ -10,17 +10,27 @@ import SpriteKit
 
 class PasswordScene: SKScene {
     
+    var passwordInput:UITextField?
+    
     override func didMove(to view: SKView) {
-        let passwordInput = UITextField(frame: CGRect(x:0, y:0, width:100, height:50))
-        passwordInput.borderStyle = UITextBorderStyle.roundedRect
-        passwordInput.backgroundColor = UIColor.darkGray
-        self.view?.addSubview(passwordInput)
+        passwordInput = UITextField(frame: CGRect(x:200, y:300, width:635, height: 50))
+        passwordInput!.borderStyle = UITextBorderStyle.roundedRect
+        passwordInput!.backgroundColor = UIColor.lightGray
+        passwordInput!.placeholder = "Enter password here"
+        passwordInput!.textColor = UIColor.white
+        passwordInput!.clearButtonMode = .always
+        passwordInput!.adjustsFontSizeToFitWidth = true
+        self.view?.addSubview(passwordInput!)
+    
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first!
         let okButton = childNode(withName: "ok")
         let cancelButton = childNode(withName: "cancel")
+        
+        passwordInput?.endEditing(true)
+        print(passwordInput!.text)
         
         if okButton!.contains(touch.location(in:self)) {
             let scoreScene = SKScene(fileNamed: "ScoreScene")
