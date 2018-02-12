993f257e7d2156fdd3a8a5482328048d73ad8eac
Eleanor Meriwether <eleanor@delux.com>
2018-02-11 16:44:37 -0800

    adjust moon and duck scene

--- DuckColoringGame/Scenes/ColoringGame/RainScene/RainScene.swift
+++ DuckColoringGame/Scenes/ColoringGame/RainScene/RainScene.swift
@@ -108,8 +108,8 @@ class RainScene: SKScene {
                 rain!.run(rainstick)
                 
                 //Variables to switch screens
-                let fadeOut = SKAction.fadeOut(withDuration:2)
-                let wait2 = SKAction.wait(forDuration: 2)
+                let fadeOut = SKAction.fadeOut(withDuration:3)
+                let wait2 = SKAction.wait(forDuration: 3)
                 let sequenceFade = SKAction.sequence([wait2, fadeOut])
                 run(sequenceFade) {
                     let duckScene = SKScene(fileNamed: "DuckScene")
