c124667baf68a1b9370753b879f6d346c9f0afc3
Eleanor Meriwether <eleanor@delux.com>
2018-02-11 16:10:17 -0800

    add rain scene

--- DuckColoringGame/Scenes/ColoringGame/RainScene/RainScene.swift
+++ DuckColoringGame/Scenes/ColoringGame/RainScene/RainScene.swift
@@ -1,5 +1,5 @@
 //
-//  HandScene.swift
+//  RainScene.swift
 //  TimeForChildrenGame
 //
 //  Created by Eleanor Meriwether on 12/7/17.
@@ -8,7 +8,7 @@
 
 import SpriteKit
 
-class HandScene: SKScene {
+class RainScene: SKScene {
     // local variables to keep track of whether instructions are playing
     var instructionsComplete = false
     var reminderComplete = true
@@ -17,8 +17,8 @@ class HandScene: SKScene {
     var sceneOver = false
     
     // local variables to keep track of touches for this scene
-    var hand_incorrectTouches = 0
-    var hand_correctTouches = 0
+    var rain_incorrectTouches = 0
+    var rain_correctTouches = 0
     var totalTouches = 0
     
     override func didMove(to view: SKView) {
@@ -39,8 +39,8 @@ class HandScene: SKScene {
         // set up sequence for if the scene has not been touched for 10 seconds: play the idle reminder
         let reminderIfIdle = SKAction.run {
             self.reminderComplete = false
-            let hand_reminder = SKAction.playSoundFileNamed("instructions_hamburger", waitForCompletion: true)
-            self.run(hand_reminder, completion: { self.reminderComplete = true} )
+            let rain_reminder = SKAction.playSoundFileNamed("instructions_hamburger", waitForCompletion: true)
+            self.run(rain_reminder, completion: { self.reminderComplete = true} )
         }
         
         // for every one second, do this action:
@@ -71,72 +71,73 @@ class HandScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // local variable for hand sprite
-        let hand = self.childNode(withName: "hand_bw")
+        // local variable for rain sprite
+        let rain = self.childNode(withName: "rain_bw")
         
         // if no instructions are playing
         if (instructionsComplete == true) && (reminderComplete == true) && (sceneOver == false){
             let touch = touches.first!
             
-            //If hand sprite's alpha mask is touched...
-            if (physicsWorld.body(at: touch.location(in: self)) == hand?.physicsBody) && (sceneOver == false) {
+            //If rain sprite's alpha mask is touched...
+            if (physicsWorld.body(at: touch.location(in: self)) == rain?.physicsBody) && (sceneOver == false) {
                 sceneOver = true
-                hand_correctTouches += 1
+                rain_correctTouches += 1
                 correctTouches += 1
                 
                 // if there weren't any incorrect touches, add to game-wide numOfCorrectFirstTry
-                if (hand_incorrectTouches == 0) {
+                if (rain_incorrectTouches == 0) {
                     numOfCorrectFirstTry += 1
                     numOfCorrectSimpleBG += 1
                     numOfCorrectSetSize4 += 1
                     
-                    correctFirstTriesArray.append("hand")
-                    correctTouchesArray.append("hand")
-                    correctSetSize4.append("hand")
-                    correctBGSimple.append("hand")
+                    correctFirstTriesArray.append("rain")
+                    correctTouchesArray.append("rain")
+                    correctSetSize4.append("rain")
+                    correctBGSimple.append("rain")
                 }
                 
-                // Change sprite to colored hand
-                let coloredhand:SKTexture = SKTexture(imageNamed: "handScene_hand_colored")
-                let changeToColored:SKAction = SKAction.animate(with: [coloredhand], timePerFrame: 0.0001)
-                hand!.run(changeToColored)
+                // Change sprite to colored rain
+                let coloredrain:SKTexture = SKTexture(imageNamed: "rainScene_rain_colored")
+                let changeToColored:SKAction = SKAction.animate(with: [coloredrain], timePerFrame: 0.0001)
+                rain!.run(changeToColored)
                 
-                //Variables for hand audio
-                let clap = SKAction.playSoundFileNamed("hand", waitForCompletion: true)
+                //Variables for rain audio
+                let rainstick = SKAction.playSoundFileNamed("rain", waitForCompletion: true)
                 
                 //Run all actions
-                hand!.run(clap)
+                rain!.run(rainstick)
                 
                 //Variables to switch screens
                 let fadeOut = SKAction.fadeOut(withDuration:2)
                 let wait2 = SKAction.wait(forDuration: 2)
                 let sequenceFade = SKAction.sequence([wait2, fadeOut])
                 run(sequenceFade) {
-                    let rainScene = SKScene(fileNamed: "RainScene")
-                    rainScene?.scaleMode = SKSceneScaleMode.aspectFill
-                    self.scene!.view?.presentScene(rainScene!)
+                    let duckScene = SKScene(fileNamed: "DuckScene")
+                    duckScene?.scaleMode = SKSceneScaleMode.aspectFill
+                    self.scene!.view?.presentScene(duckScene!)
                 }
             }
             else {
-                hand_incorrectTouches += 1
+                rain_incorrectTouches += 1
                 incorrectTouches += 1
                 
                 // Play wrong noise
                 let wrong = SKAction.playSoundFileNamed("wrong", waitForCompletion: true)
-                hand?.run(wrong)
+                rain?.run(wrong)
             }
             
             // play reminder instructions if user has touched screen 3 times incorrectly
-            if (hand_incorrectTouches % 3 == 0) && hand_correctTouches < 1 {
+            if (rain_incorrectTouches % 3 == 0) && rain_correctTouches < 1 {
                 reminderComplete = false
-                let hand_reminder = SKAction.playSoundFileNamed("instructions_hamburger", waitForCompletion: true)
-                run(hand_reminder, completion: { self.reminderComplete = true} )
+                let rain_reminder = SKAction.playSoundFileNamed("instructions_hamburger", waitForCompletion: true)
+                run(rain_reminder, completion: { self.reminderComplete = true} )
             }
         }
         // update totalTouches variable for idle reminder
-        totalTouches = hand_correctTouches + hand_incorrectTouches
+        totalTouches = rain_correctTouches + rain_incorrectTouches
     }
 }
+
 
 
 
