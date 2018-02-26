115e4e236216153df8009f98a5c713e5ef8cccdc
Eleanor Meriwether <eleanor@delux.com>
2018-02-26 12:23:45 -0800

    add nameScene to collect user's name

---
+++ DuckColoringGame/Scenes/StartScenes/NameScene/NameScene.swift
@@ -0,0 +1,9 @@
+//
+//  NameScene.swift
+//  DuckColoringGame
+//
+//  Created by Eleanor Meriwether on 2/26/18.
+//  Copyright © 2018 Eleanor Meriwether. All rights reserved.
+//
+
+import Foundation
