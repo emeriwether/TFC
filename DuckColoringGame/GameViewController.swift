b8137ace2a3b5f726785833b7d2ef260bc39d4f0
Eleanor Meriwether <eleanor@delux.com>
2018-02-25 21:13:55 -0800

    start game at startScene again

--- DuckColoringGame/GameViewController.swift
+++ DuckColoringGame/GameViewController.swift
@@ -14,8 +14,8 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "MouseScene") {
+            // Load the SKScene from 'StartScene'
+            if let scene = SKScene(fileNamed: "StartScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
