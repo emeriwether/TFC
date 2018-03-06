0106b8be659bf99175459233251a00d045babe74
Eleanor Meriwether <eleanor@delux.com>
2018-03-06 08:26:50 -0800

    Revert "add nameScene to collect user's name"
    This reverts commit 115e4e236216153df8009f98a5c713e5ef8cccdc.

--- DuckColoringGame/Scenes/StartScenes/NameScene/NameScene.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NameScene.swift
-//  DuckColoringGame
-//
-//  Created by Eleanor Meriwether on 2/26/18.
-//  Copyright © 2018 Eleanor Meriwether. All rights reserved.
-//
-
-import Foundation
