e4b2b3a752c7c166665c5245b67425afb81762cc
emeriwether <eleanor@delux.com>
2018-03-13 10:23:36 -0700

    ct prev com

--- DuckColoringGame/Scenes/EndGameScenes/ScoreScene4/ScoreScene4.swift
+++ DuckColoringGame/Scenes/EndGameScenes/ScoreScene4/ScoreScene4.swift
@@ -148,7 +148,6 @@ class ScoreScene4: SKScene {
         
         // if back button is touched
         if backButton!.contains(touch.location(in: self)) {
-            resetAllGameStats_coloring()
             transitionScene (currentScene: self, sceneString: "ScoreScene3", waitTime: 0, fadeTime: 0)
         }
     }
